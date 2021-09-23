35a52684ad05c1aac5272e53827e158afad29b83
Erik Perri <erik.perri@sourcetoad.com>
2021-09-22 21:15:27 -0400

    test: Added more tests

--- tests/Unit/RuleTest.php
+++ tests/Unit/RuleTest.php
@@ -6,6 +6,10 @@ namespace Sourcetoad\RuleHelper\Tests\Unit;
 
 use Carbon\CarbonImmutable;
 use DateTime;
+use Illuminate\Auth\AuthManager;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Hashing\Hasher;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rules\RequiredIf;
@@ -24,14 +28,14 @@ class RuleTest extends TestCase
     /**
      * @dataProvider ruleDataProvider
      */
-    public function testRuleIntegration($data, callable $rules, bool $fails, ?array $errors = null): void
+    public function testRuleIntegration($data, \Closure $rules, bool $fails, ?array $errors = null): void
     {
         // Arrange
         if (!is_array($data)) {
             $data = ['field' => $data];
         }
 
-        $rules = $rules();
+        $rules = $rules->call($this);
         if (!is_array($rules)) {
             $rules = ['field' => $rules];
         }
@@ -247,6 +251,372 @@ class RuleTest extends TestCase
                     ],
                 ],
             ],
+            'before valid' => [
+                'data' => '2021-01-01',
+                'rules' => fn() => RuleSet::create()->before('2021-01-02'),
+                'fails' => false,
+            ],
+            'before invalid' => [
+                'data' => '2021-01-02',
+                'rules' => fn() => RuleSet::create()->before('2021-01-02'),
+                'fails' => true,
+            ],
+            'before valid with DateTime' => [
+                'data' => '2021-01-01',
+                'rules' => fn() => RuleSet::create()->before(new DateTime('2021-01-02')),
+                'fails' => false,
+            ],
+            'before invalid with DateTime' => [
+                'data' => '2021-01-02',
+                'rules' => fn() => RuleSet::create()->before(new DateTime('2021-01-02')),
+                'fails' => true,
+            ],
+            'before valid with Carbon' => [
+                'data' => '2021-01-01',
+                'rules' => fn() => RuleSet::create()->before(CarbonImmutable::parse('2021-01-02')),
+                'fails' => false,
+            ],
+            'before invalid with Carbon' => [
+                'data' => '2021-01-02',
+                'rules' => fn() => RuleSet::create()->before(CarbonImmutable::parse('2021-01-02')),
+                'fails' => true,
+            ],
+            'beforeOrEqual valid' => [
+                'data' => '2021-01-01',
+                'rules' => fn() => RuleSet::create()->beforeOrEqual('2021-01-01'),
+                'fails' => false,
+            ],
+            'beforeOrEqual invalid' => [
+                'data' => '2021-01-02',
+                'rules' => fn() => RuleSet::create()->beforeOrEqual('2021-01-01'),
+                'fails' => true,
+            ],
+            'beforeOrEqual valid with DateTime' => [
+                'data' => '2021-01-01',
+                'rules' => fn() => RuleSet::create()->beforeOrEqual(new DateTime('2021-01-01')),
+                'fails' => false,
+            ],
+            'beforeOrEqual invalid with DateTime' => [
+                'data' => '2021-01-02',
+                'rules' => fn() => RuleSet::create()->beforeOrEqual(new DateTime('2021-01-01')),
+                'fails' => true,
+            ],
+            'between valid with string' => [
+                'data' => str_repeat('.', 10),
+                'rules' => fn() => RuleSet::create()->between(9, 11),
+                'fails' => false,
+            ],
+            'between invalid with string' => [
+                'data' => str_repeat('.', 12),
+                'rules' => fn() => RuleSet::create()->between(9, 11),
+                'fails' => true,
+            ],
+            'boolean valid' => [
+                'data' => '1',
+                'rules' => fn() => RuleSet::create()->boolean(),
+                'fails' => false,
+            ],
+            'boolean invalid' => [
+                'data' => 'please',
+                'rules' => fn() => RuleSet::create()->boolean(),
+                'fails' => true,
+            ],
+            'confirmed valid' => [
+                'data' => [
+                    'field' => 'value',
+                    'field_confirmation' => 'value',
+                ],
+                'rules' => fn() => [
+                    'field' => RuleSet::create()->confirmed(),
+                ],
+                'fails' => false,
+            ],
+            'confirmed invalid' => [
+                'data' => [
+                    'field' => 'value',
+                    'field_confirmation' => 'other-value',
+                ],
+                'rules' => fn() => [
+                    'field' => RuleSet::create()->confirmed(),
+                ],
+                'fails' => true,
+            ],
+            'currentPassword valid' => [
+                'data' => 'password-one',
+                'rules' => function () {
+                    $this->mockUserAuth('password-one', null);
+
+                    return RuleSet::create()->currentPassword();
+                },
+                'fails' => false,
+            ],
+            'currentPassword invalid' => [
+                'data' => 'password-one',
+                'rules' => function () {
+                    $this->mockUserAuth('password-two', null);
+
+                    return RuleSet::create()->currentPassword();
+                },
+                'fails' => true,
+            ],
+            'currentPassword with guard valid' => [
+                'data' => 'password-one',
+                'rules' => function () {
+                    $this->mockUserAuth('password-one', 'guardName');
+
+                    return RuleSet::create()->currentPassword('guardName');
+                },
+                'fails' => false,
+            ],
+            'currentPassword with guard invalid' => [
+                'data' => 'password-a',
+                'rules' => function () {
+                    $this->mockUserAuth('password-b', 'guardName');
+
+                    return RuleSet::create()->currentPassword('guardName');
+                },
+                'fails' => true,
+            ],
+            'date valid' => [
+                'data' => '2021-01-01',
+                'rules' => fn() => RuleSet::create()->date(),
+                'fails' => false,
+            ],
+            'date invalid' => [
+                'data' => 'a',
+                'rules' => fn() => RuleSet::create()->date(),
+                'fails' => true,
+            ],
+            'dateEquals valid' => [
+                'data' => '01-Jan-2021',
+                'rules' => fn() => RuleSet::create()->dateEquals('2021-01-01'),
+                'fails' => false,
+            ],
+            'dateEquals invalid' => [
+                'data' => '02-Jan-2021',
+                'rules' => fn() => RuleSet::create()->dateEquals('2021-01-01'),
+                'fails' => true,
+            ],
+            'dateFormat valid' => [
+                'data' => '01-Jan-2021',
+                'rules' => fn() => RuleSet::create()->dateFormat('d-M-Y'),
+                'fails' => false,
+            ],
+            'dateFormat invalid' => [
+                'data' => '2021-01-01',
+                'rules' => fn() => RuleSet::create()->dateFormat('d-M-Y'),
+                'fails' => true,
+            ],
+            'different valid' => [
+                'data' => [
+                    'field-a' => 'a',
+                    'field-b' => 'b',
+                ],
+                'rules' => fn() => [
+                    'field-a' => RuleSet::create()->different('field-b'),
+                ],
+                'fails' => false,
+            ],
+            'different invalid' => [
+                'data' => [
+                    'field-a' => 'a',
+                    'field-b' => 'a',
+                ],
+                'rules' => fn() => [
+                    'field-a' => RuleSet::create()->different('field-b'),
+                ],
+                'fails' => true,
+            ],
+            'digits valid' => [
+                'data' => '02021',
+                'rules' => fn() => RuleSet::create()->digits(5),
+                'fails' => false,
+            ],
+            'digits invalid' => [
+                'data' => 'a2021',
+                'rules' => fn() => RuleSet::create()->digits(5),
+                'fails' => true,
+            ],
+            'digitsBetween valid' => [
+                'data' => '2021',
+                'rules' => fn() => RuleSet::create()->digitsBetween(2, 5),
+                'fails' => false,
+            ],
+            'digitsBetween invalid' => [
+                'data' => '2002021',
+                'rules' => fn() => RuleSet::create()->digitsBetween(2, 5),
+                'fails' => true,
+            ],
+            'distinct valid' => [
+                'data' => [
+                    'field' => ['a', 'b', 'c', 'A'],
+                ],
+                'rules' => fn() => [
+                    'field.*' => RuleSet::create()->distinct(),
+                ],
+                'fails' => false,
+            ],
+            'distinct invalid' => [
+                'data' => [
+                    'field' => ['a', 'b', 'c', '1', 1],
+                ],
+                'rules' => fn() => [
+                    'field.*' => RuleSet::create()->distinct(),
+                ],
+                'fails' => true,
+            ],
+            'distinct strict valid' => [
+                'data' => [
+                    'field' => ['a', '1', 1],
+                ],
+                'rules' => fn() => [
+                    'field.*' => RuleSet::create()->distinct(true),
+                ],
+                'fails' => false,
+            ],
+            'distinct strict invalid' => [
+                'data' => [
+                    'field' => ['a', 'b', 'c', 1, 1],
+                ],
+                'rules' => fn() => [
+                    'field.*' => RuleSet::create()->distinct(true),
+                ],
+                'fails' => true,
+            ],
+            'distinct ignoreCase valid' => [
+                'data' => [
+                    'field' => ['a', 'b', 'c'],
+                ],
+                'rules' => fn() => [
+                    'field.*' => RuleSet::create()->distinct(false, true),
+                ],
+                'fails' => false,
+            ],
+            'distinct ignoreCase invalid' => [
+                'data' => [
+                    'field' => ['a', 'b', 'c', 'A'],
+                ],
+                'rules' => fn() => [
+                    'field.*' => RuleSet::create()->distinct(false, true),
+                ],
+                'fails' => true,
+            ],
+            'email valid' => [
+                'data' => 'someone@example.com',
+                'rules' => fn() => RuleSet::create()->email(),
+                'fails' => false,
+            ],
+            'email invalid' => [
+                'data' => 'someone',
+                'rules' => fn() => RuleSet::create()->email(),
+                'fails' => true,
+            ],
+            'email rfc valid' => [
+                'data' => 'someone@'.Str::repeat('example', 100).'.com',
+                'rules' => fn() => RuleSet::create()->email('rfc'),
+                'fails' => false,
+            ],
+            'email rfc invalid' => [
+                'data' => 'someone',
+                'rules' => fn() => RuleSet::create()->email('rfc'),
+                'fails' => true,
+            ],
+            'email strict valid' => [
+                'data' => 'someone@example.com',
+                'rules' => fn() => RuleSet::create()->email('strict'),
+                'fails' => false,
+            ],
+            'email strict invalid' => [
+                'data' => 'someone@'.Str::repeat('example', 100).'.com',
+                'rules' => fn() => RuleSet::create()->email('strict'),
+                'fails' => true,
+            ],
+            'email dns valid' => [
+                'data' => 'someone@gmail.com',
+                'rules' => fn() => RuleSet::create()->email('dns'),
+                'fails' => false,
+            ],
+            'email dns invalid' => [
+                'data' => 'someone@'.Str::repeat(Str::uuid()->toString(), 3).'.com',
+                'rules' => fn() => RuleSet::create()->email('dns'),
+                'fails' => true,
+            ],
+            'email spoof valid' => [
+                'data' => 'someone@gmail.com',
+                'rules' => fn() => RuleSet::create()->email('spoof'),
+                'fails' => false,
+            ],
+            'email spoof invalid' => [
+                'data' => "someone@\u{0430}pple.com",
+                'rules' => fn() => RuleSet::create()->email('spoof'),
+                'fails' => true,
+            ],
+            'email filter valid' => [
+                'data' => 'someone@gmail.com',
+                'rules' => fn() => RuleSet::create()->email('filter'),
+                'fails' => false,
+            ],
+            'email filter invalid' => [
+                'data' => "someone@com",
+                'rules' => fn() => RuleSet::create()->email('filter'),
+                'fails' => true,
+            ],
+            'endsWith valid' => [
+                'data' => 'string',
+                'rules' => fn() => RuleSet::create()->endsWith('g'),
+                'fails' => false,
+            ],
+            'endsWith invalid' => [
+                'data' => 'string',
+                'rules' => fn() => RuleSet::create()->endsWith('a'),
+                'fails' => true,
+            ],
+            'endsWith any valid' => [
+                'data' => 'string-c',
+                'rules' => fn() => RuleSet::create()->endsWith('a', 'b', 'c'),
+                'fails' => false,
+            ],
+            'endsWith any invalid' => [
+                'data' => 'string-d',
+                'rules' => fn() => RuleSet::create()->endsWith('a', 'b', 'c'),
+                'fails' => true,
+            ],
+
+
+            'max valid with string' => [
+                'data' => str_repeat('.', 10),
+                'rules' => fn() => RuleSet::create()->max(10),
+                'fails' => false,
+            ],
+            'max invalid with string' => [
+                'data' => str_repeat('.', 11),
+                'rules' => fn() => RuleSet::create()->max(10),
+                'fails' => true,
+            ],
+            'max valid with array' => [
+                'data' => [
+                    'field' => ['a', 'b', 'c'],
+                ],
+                'rules' => fn() => RuleSet::create()->max(3),
+                'fails' => false,
+            ],
+            'max invalid with array' => [
+                'data' => [
+                    'field' => ['a', 'b', 'c'],
+                ],
+                'rules' => fn() => RuleSet::create()->max(2),
+                'fails' => true,
+            ],
+            'string valid' => [
+                'data' => 'string',
+                'rules' => fn() => RuleSet::create()->string(),
+                'fails' => false,
+            ],
+            'string invalid' => [
+                'data' => 1,
+                'rules' => fn() => RuleSet::create()->string(),
+                'fails' => true,
+            ],
         ];
     }
 
@@ -419,5 +789,42 @@ class RuleTest extends TestCase
                 'fails' => true,
             ],
         ];
+    }
+
+    private function mockUserAuth(string $password, ?string $guardName): void
+    {
+        $user = new User();
+        /** @noinspection PhpUndefinedFieldInspection */
+        $user->password = $password;
+
+        $hasher = $this->mock(Hasher::class)
+            ->expects('check')
+            ->once()
+            ->andReturnUsing(function ($value, $userPassword) {
+                return $value === $userPassword;
+            })
+            ->getMock();
+
+        $guardMock = $this->mock(Guard::class);
+
+        $guardMock
+            ->expects('guest')
+            ->once()
+            ->andReturn(false);
+
+        $guardMock
+            ->expects('user')
+            ->once()
+            ->andReturn($user);
+
+        $authManager = $this->mock(AuthManager::class)
+            ->expects('guard')
+            ->once()
+            ->with($guardName)
+            ->andReturn($guardMock)
+            ->getMock();
+
+        $this->app->instance('auth', $authManager);
+        $this->app->instance('hash', $hasher);
     }
 }
