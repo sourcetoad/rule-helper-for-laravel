c0c3fee480f7aff861b7a4092afb901ed3323a13
Erik Perri <erik.perri@sourcetoad.com>
2021-10-07 08:46:09 -0400

    fix: Added `acceptedIf` to contract

--- src/Contracts/RuleSet.php
+++ src/Contracts/RuleSet.php
@@ -11,6 +11,7 @@ interface RuleSet
     public static function create(array $rules = []): self;
     public function concat(...$rule): self;
     public function accepted(): self;
+    public function acceptedIf(): self;
     public function activeUrl(): self;
     public function after($date): self;
     public function afterOrEqual($date): self;
